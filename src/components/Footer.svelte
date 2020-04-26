7ae7b08019539a061b8027a61f252b5e626896da
darkp0lx <polbarazorda@gmail.com>
2020-04-26 18:45:00 -0500

    first commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<div class="Footer">
+    <div class="Footer-content">
+      <div class="Footer-copy">
+        © 2020 PUGSTAGRAM FROM PxndxX
+      </div>
+    </div>
+</div>
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
---
+++ src/containers/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import {onMount} from "svelte";
+  import Header from '../components/Header.svelte'
+  import Main from '../components/Main.svelte'
+  import SideBar from '../components/SideBar.svelte'
+  import TimeLine from '../components/TimeLine.svelte'
+  let data={
+    }
+  const API = 'https://us-central1-pugstagram-co.cloudfunctions.net/data';
+  onMount(async()=>{
+    const responsive= await fetch(API);
+    data= await responsive.json();
+  })
+</script>
+
+<div class="main">
+<Header/>
+<Main>
+  <TimeLine  posts={data.posts}/>
+  <SideBar {...data.user}/>  
+</Main>
+
+</div>
+
+<style>
+    :global(body) {
+    background-color: #fafafa;
+    color: rgba(38, 38, 38, 0.7);
+    font-family: "Lato", sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+  :global(h1, h2, h3) {
+    margin: 0;
+    padding: 0;
+  }
+</style>
